ee53027a6d9c66df68f9129fd4b29ca5d5f111a8
shivam-handsintechnology <shivam@handsintechnology.com>
2024-04-13 19:05:50 +0530

    Update dependencies and URLs in package.json and index.js

--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@
 
 const logger = require('./logger/logger');
 const apirouter = require('./routes')
-const NodeMonitor=require("monitornodejstestversion")
+const path=require("path")
 const checkVerification = require('./middlewares/verifyClient')
 const { DBConnection } = require("./config/connection");
 const JsSnippetController = require('./controllers/JsSnippetController');
-const cors = require("cors"), fileUpload = require('express-fileupload'), express = require("express"); hpp = require('hpp'), morgan = require('morgan'), helmet = require('helmet'), bodyParser = require('body-parser'), dotenv = require('dotenv'), cluster = require("cluster"), os = require("os"), numCPUs = os.cpus().length, process = require("process");
+const cors = require("cors"), fileUpload = require('express-fileupload'), express = require("express"); hpp = require('hpp'), helmet = require('helmet'), dotenv = require('dotenv'), cluster = require("cluster"), os = require("os"), numCPUs = os.cpus().length, process = require("process");
 // Connected to mongodb
 dotenv.config();
 DBConnection(process.env.MONGO_URI)
 // Create Express APP
 const app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb", extended: true }));
 // File Upload Functionality
 app.use(fileUpload({
   limits: { fileSize: 50 * 1024 * 1024 },
@@ -26,18 +26,23 @@ app.set('trust proxy', 1) // trust first proxy
 app.use(hpp());
 app.use(cors())
 app.use(helmet())
-app.use(morgan('dev'))
 
 app.disable('x-powered-by');
 app.disable('etag');
-app.use(NodeMonitor.validateAndSetMiddleware("8dae6ee9-ad81-417a-93a0-f60a7e9e570c"))
-app.use(apirouter)
-app.use(NodeMonitor.testing)
+app.use("/api",apirouter)
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+ return res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+});
 // Error handling middleware
 app.use((err, req, res, next) => {
   logger.error(err.stack);
-  res.status(500).json({message:'Something broke!'});
+ let statusCode= err.statusCode || 500;
+ let message= err.message || 'Something broke!';
+  return res.status(statusCode).json({ message });
 });
+
 // 404 Error handling middleware
 app.use((req, res) => {
     // Render the welcome page (views/welcome.ejs)

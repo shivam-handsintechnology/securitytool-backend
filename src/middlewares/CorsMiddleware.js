dc001b7e34f10a5c1d0e83939500cd0244beba1e
shivam-handsintechnology <shivam@handsintechnology.com>
2024-04-24 15:18:56 +0530

    Refactor WeakCrossDomainPolicy controller and middleware, update routes, and fix CORS middleware allowOrigin value

--- src/middlewares/CorsMiddleware.js
+++ src/middlewares/CorsMiddleware.js
@@ -9,6 +9,7 @@ const SkipApi = [
 module.exports = async function cors(req, res, next) {
  
   // Set specific CORS headers
+  res.header("Access-Control-Allow-Origin", "https://securitytool-front.handsintechnology.in");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
  console.log("Original Url",req.path)
@@ -21,14 +22,7 @@ module.exports = async function cors(req, res, next) {
   // Check if the request has an origin header
  if (!req.headers.origin) {
     return errorHandler(res, 403, "Forbidden");
-  } else {
-    // Check if the origin is in the allowed origins list
-    if (allowOrigin.includes(req.headers.origin)) {
-      res.header("Access-Control-Allow-Origin", req.headers.origin);
-    } else {
-      return errorHandler(res, 403, "Forbidden");
-    }
-  }
+  } 
 
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
--- src/routes/Security/WeakCrossDomainPolicy.route.js
+++ src/routes/Security/WeakCrossDomainPolicy.route.js
@@ -2,5 +2,7 @@ const router= require('express').Router();
 const WeakCrossDomainPolicy=require('../../controllers/Security/WeakCrossDomainPolicy.controller');
 
 // Is "Origin" header in client request validated at the server?
-router.get("/is-origin-header-validated",WeakCrossDomainPolicy.isOriginHeaderValidated);
+router.get("/OriginHeaderValidation",WeakCrossDomainPolicy.isOriginHeaderValidated);
+// Is "Access-Control-Allow-Origin" header in server response is set securely?
+router.get("/AccessControlAllowOriginHeaderSecure",WeakCrossDomainPolicy.isAccessControlAllowOriginHeaderSecure);
 module.exports=router;

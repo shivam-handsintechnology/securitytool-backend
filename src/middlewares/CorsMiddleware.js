298fc5a1d7a97679254fa0bb938488cdf5a841c8
shivam-handsintechnology <shivam@handsintechnology.com>
2024-05-01 11:39:51 +0530

    Refactor CorsMiddleware.js to update CORS headers and allow origin based on request path

--- src/middlewares/CorsMiddleware.js
+++ src/middlewares/CorsMiddleware.js
@@ -2,10 +2,10 @@ const { errorHandler } = require("../utils/errorHandler");
 
 module.exports = async function cors(req, res, next) {
   const Origin= process.env.NODE_ENV==="production"?"https://securitytool-front.handsintechnology.in":"http://localhost:3000"
-
+  
   // Set specific CORS headers
   // res.header("Access-Control-Allow-Origin", "https://securitytool-front.handsintechnology.in");
-  res.header("Access-Control-Allow-Origin", Origin);
+  res.header("Access-Control-Allow-Origin",req.path.includes("api/client")?"*": Origin);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   // Check if the request has an origin header

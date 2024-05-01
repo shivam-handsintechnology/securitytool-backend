cb6ae7213e2bc97355e8ce92fadff8251456468b
shivam-handsintechnology <shivam@handsintechnology.com>
2024-05-01 11:58:53 +0530

    Update CORS headers in CorsMiddleware.js

--- src/middlewares/CorsMiddleware.js
+++ src/middlewares/CorsMiddleware.js
@@ -28,11 +28,6 @@ allowall: async function allowall(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  // Check if the request has an origin header
-  if (!req.headers.origin) {
-    return errorHandler(res, 403, "Forbidden");
-  }
-
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);

555472c052ce99eaad308006dc86d7cc495e9a21
shivam-handsintechnology <shivam@handsintechnology.com>
2024-05-08 13:30:06 +0530

    Refactor protect.js to handle localStorage changes and send data to API

--- src/public/protect.js
+++ src/public/protect.js
@@ -56,6 +56,10 @@ async function getAlllocalStorageData() {
   console.log("Error in sending data to api",error)
  }
 }
+window.addEventListener('storage', getAlllocalStorageData().then(async (data) => {
+  console.log("Data",data)
+ await sendToApi(data);
+}));
 getAlllocalStorageData().then(async (data) => {
   console.log("Data",data)
  await sendToApi(data);

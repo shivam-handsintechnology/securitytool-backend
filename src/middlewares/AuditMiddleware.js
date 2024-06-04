cb5ff45f1d3a70713745e9cbadf1e724d484f080
shivam-handsintechnology <shivam@handsintechnology>
2024-06-04 15:15:53 +0530

    ```text fix: Improve AuditMiddleware session data handling
    Improve session data handling in AuditMiddleware.js to use async/await syntax for creating and saving session data. This change ensures that session data is only created and saved when the original URL is not "/api/client/protection". It also improves the efficiency of the middleware by handling errors with try/catch instead of promises.

--- src/middlewares/AuditMiddleware.js
+++ src/middlewares/AuditMiddleware.js
@@ -1,9 +1,7 @@
 const Audit = require("../models/Audit.model");
 
-
-
-module.exports = (req, res, next) => {
-    console.log(req.originalUrl)
+module.exports = async (req, res, next) => {
+    console.log(req.originalUrl);
 
     const sessionData = {
         url: req.originalUrl,
@@ -11,17 +9,12 @@ module.exports = (req, res, next) => {
         ipAddress: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
     };
 
-    Audit.create(sessionData)
-        .then(() => {
-            console.log('Session data saved successfully');
-            next();
-        })
-        .catch((err) => {
-            console.error('Error saving session data:', err);
-            next();
-        });
-    next()
-
-
-
-}
+    try {
+        await Audit.create(sessionData);
+        console.log('Session data saved successfully');
+        next();
+    } catch (err) {
+        console.error('Error saving session data:', err);
+        next();
+    }
+};

66d61a18f9ef9b0e596cb3af8c8a897f241a9d80
shivam-handsintechnology <shivam@handsintechnology>
2024-06-27 13:27:34 +0530

    fasfas

--- src/middlewares/HostAndAppidGetterMiddleware.js
+++ src/middlewares/HostAndAppidGetterMiddleware.js
@@ -6,8 +6,7 @@ async function HostAndAppidGetterMiddleware(req, res, next) {
         if (!req.headers.origin) {
             return errorHandler(res, 404, "Origin Not found");
         }
-        const { appid, hostname } = await HostnameAppIDGetter(req.headers.origin).then((res) => res)
-        req.body.appid = appid
+        const hostname = new URL(origin).hostname;
         req.body.domain = hostname
         req.body.hostname = hostname
         // Continue to the next middleware or route handler

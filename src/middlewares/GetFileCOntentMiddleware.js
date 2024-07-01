ada5c37ddc3d853c6ad670a33c730dcc065fcb85
shivam-handsintechnology <shivam@handsintechnology>
2024-07-01 18:09:27 +0530

    securitytool

--- src/middlewares/GetFileCOntentMiddleware.js
+++ src/middlewares/GetFileCOntentMiddleware.js
@@ -1,30 +1,30 @@
 const { errorHandler } = require("../utils/errorHandler")
 const axios = require("axios")
 module.exports = async (req, res, next) => {
-        try {
-            let payload = { ...req.body, ...req.query, ...req.params }
-            let domain =payload.domain
-            let response = await axios.get(`http://${domain}/fileContent`,{
-                headers: {
-                    'origin': "https://securitytool.handsintechnology.in",
-                }
-            }).then(response => response).catch(error => error.response)
-            console.log({response:response.status})
-            if (response.status == 200) {
-                if(Array.isArray(response.data.data)){
-                    req.body.fileContent = response.data.data
-                }
-                if(!Array.isArray(response.data.data)){
-                    throw new Error("Invalid  Content")
-                }
-                
-            }else if(response.status===502){
-                throw new Error("Bad Gateway")
-            }else if (response.status >= 400) {
-                throw new Error("Access Denied")
+    try {
+        let payload = { ...req.body, ...req.query, ...req.params }
+        let domain = payload.domain
+        let response = await axios.get(`http://${domain}/securitytool/fileContent`, {
+            headers: {
+                'origin': "https://securitytool.handsintechnology.in",
             }
-            next()
-        } catch (error) {
-            return errorHandler(res, 500, error.message)
+        }).then(response => response).catch(error => error.response)
+        console.log({ response: response.status })
+        if (response.status == 200) {
+            if (Array.isArray(response.data.data)) {
+                req.body.fileContent = response.data.data
+            }
+            if (!Array.isArray(response.data.data)) {
+                throw new Error("Invalid  Content")
+            }
+
+        } else if (response.status === 502) {
+            throw new Error("Bad Gateway")
+        } else if (response.status >= 400) {
+            throw new Error("Access Denied")
         }
+        next()
+    } catch (error) {
+        return errorHandler(res, 500, error.message)
     }
+}
